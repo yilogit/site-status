8cb5d5c772452be15c9a2c8876ac314fd3b7718c
yilogit <168342875+yilogit@users.noreply.github.com>
2024-05-07 23:42:52 +0800

    更新 footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          <CustomLink to="https://www.yiloko.asia" text="yiloko" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
